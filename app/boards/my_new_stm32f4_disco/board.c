4e4f54d7f7eedb336a1be3fb3393e85b88330dca
Akshay <akshay.ch.92@gmail.com>
2026-04-29 22:37:52 +0530

    L5 task2

---
+++ app/boards/my_new_stm32f4_disco/board.c
@@ -0,0 +1,18 @@
+#include <zephyr/devicetree.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <soc.h>
+
+static int board_init(void)
+{
+    printf("Board Initialised \n");
+
+    return 0;
+}
+
+/*
+ * Because platform is using ARM SCMI, drivers like scmi, mbox etc. are
+ * initialized during PRE_KERNEL_1. Common init hooks is not able to use.
+ * SoC early init and board early init could be run during PRE_KERNEL_2 instead.
+ */
+SYS_INIT(board_init, PRE_KERNEL_2, 10);
